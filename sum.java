a0146d2429dc24e2ec18518b058d648b75c3aada
sagar <swankhede639@gmail.com>
2022-03-31 12:46:22 +0530

    initial commit

--- sum.java
+++ sum.java
@@ -1,9 +1,7 @@
 public class Sum{
-
-public static void main(String[] args){
-int a =5;
-int b =6;
-
-System.out.println("the sum is" + (a+b);
-}
+    public static void main(String[] args){
+        int a = 5
+        int b = 6;
+        System.out.println(a+b);
+    }
 }
